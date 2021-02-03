b28db65fa84eaa5f30f774584e07b22d7c052cdc
RudyPark3091 <grayblack313@gmail.com>
2021-02-04 01:35:01 +0900

    Add pagination effect

---
+++ js/paginator.js
@@ -0,0 +1,30 @@
+class Paginator {
+  constructor(onScroll) {
+    this.onScroll = onScroll
+    // HTML elements being paginated
+    this.$items = [];
+    // length of item;
+    this.count = 0;
+    // present scroll amount
+    this.H = 0;
+    // amount of pixel to activate scroll
+    this.OFFSET = 1;
+    // variable for scroll debouncing
+    this.tick = 0;
+
+    document.body.onscroll = onScroll;
+  }
+
+  add($item) {
+    this.$items.push($item);
+    this.count += 1;
+  }
+
+  init() {
+    this.$items.forEach(item => {
+      item.classList.add("paginated");
+    });
+  }
+}
+
+export default Paginator;
--- js/renderer.js
+++ js/renderer.js
@@ -1,12 +1,45 @@
 import Calendar from "./calendar.js";
+import Paginator from "./paginator.js";
 
 class Renderer {
   constructor($target) {
+    this.$paginator = new Paginator(e => {
+      document.body.style.overflowY = "scroll";
+      if (window.scrollY - this.$paginator.H > this.$paginator.OFFSET) {
+        document.body.style.overflowY = "hidden";
+      } else if (this.$paginator.H - window.scrollY > this.$paginator.OFFSET) {
+        document.body.style.overflowY = "hidden";
+      }
+
+      if (this.$paginator.tick) clearTimeout(this.$paginator.tick);
+
+      this.$paginator.tick = setTimeout(_ => {
+        console.log("operation here");
+        if (
+          window.scrollY > this.$paginator.H &&
+          this.$paginator.H < window.innerHeight * this.$paginator.count
+        ) {
+          // scrolling down
+          this.$paginator.H += window.innerHeight;
+        } else if (window.scrollY < this.$paginator.H && this.$paginator.H > 0) {
+          // scrolling up
+          this.$paginator.H -= window.innerHeight;
+        }
+
+        window.scroll({
+          top: this.$paginator.H,
+          behavior: "smooth"
+        });
+      }, 200);
+    });
     this.$calendar = new Calendar(document.querySelector("#calendar"));
   }
 
   render() {
     this.$calendar.render();
+    this.$paginator.add(this.$calendar.$target);
+    this.$paginator.add(document.querySelector("#todo"));
+    this.$paginator.init();
   }
 }
 
