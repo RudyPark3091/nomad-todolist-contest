dd6f8d8563df2974b2b374d71e7050c7cfb99c38
RudyPark3091 <grayblack313@gmail.com>
2021-03-08 02:40:23 +0900

    Refactor scrolling methology
    removed scroll event listener
    added css scroll-snap-*
    updated pages from div to section

--- js/paginator.js
+++ js/paginator.js
@@ -7,15 +7,6 @@ class Paginator {
     this.$items = [];
     // length of $items;
     this.count = 0;
-    // presents scroll amount
-    this.H = 0;
-    // amount of pixel to activate scroll
-    this.OFFSET = 1;
-    // variable for scroll debouncing
-    this.tick = null;
-    this.isChrome = navigator.userAgent.indexOf("Chrome") !== -1;
-
-    this.onScroll = this.onScroll.bind(this);
   }
 
   add($item) {
@@ -24,38 +15,9 @@ class Paginator {
   }
 
   init() {
-    this.$items.forEach(item => {
+    this.$items.forEach((item) => {
       item.classList.add("paginated");
     });
-
-    document.body.onscroll = this.onScroll;
-  }
-
-  onScroll() {
-    let scroll = !this.tick;
-    clearTimeout(this.tick);
-    document.body.style.overflow = "hidden";
-
-    this.tick = setTimeout(_ => {
-      this.tick = null
-      document.body.style.overflow = "scroll";
-    }, this.isChrome ? 500 : 1000);
-
-    if (scroll) {
-      if (
-        window.scrollY - this.H >= this.OFFSET &&
-        window.scrollY > this.H
-      ) {
-        this.H += window.innerHeight;
-      } else if (
-        this.H - window.scrollY >= this.OFFSET &&
-        window.scrollY < this.H
-      ) {
-        this.H -= window.innerHeight;
-      }
-
-      window.scroll({ top: this.H, behavior: "smooth" });
-    }
   }
 }
 
